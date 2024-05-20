e47141a912f6aef1aed1411db9a29a6c969add12
Abhinav Attaluri <attaluriabhi@gmail.com>
2024-05-20 16:20:21 -0400

    Updated all root components

--- School Dashboard/src/roots/Dashboard.jsx
+++ School Dashboard/src/roots/Dashboard.jsx
@@ -1,5 +1,17 @@
-import { React } from 'react';
+import React from 'react';
+
 
 export const Dashboard = () => {
+
     
+    return (
+        <> 
+            <div className="navigation"> 
+                {/* Add Navigation-Bar Component */}
+            </div>
+            <h1> Dashboard </h1>
+
+        </>
+    )
+
 }
--- School Dashboard/src/roots/Home.jsx
+++ School Dashboard/src/roots/Home.jsx
@@ -1,4 +1,5 @@
-import { React, useState } from 'react'
+import React from 'react';
+import { useState } from 'react';
 import { Link } from 'react-router-dom';
 
 export const Home = () => {
@@ -7,9 +8,13 @@ export const Home = () => {
         <> 
             <h1> Welcome Page </h1>
             <Link to={"/directory"}> Directory </Link>
+            <br/>
             <Link to={"/dashboard"}> Dashboard </Link>
+            <br/>
             <Link to={"/class"}> Class Page </Link>
+            <br/>
             <Link to={"/calendar"}> Calendar </Link>
+            <br/>
             
         </>
     )

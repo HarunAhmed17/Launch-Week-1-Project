26256a847fe4dd4585a924e8b1dbd5c11ce6c814
Abhinav Attaluri <attaluriabhi@gmail.com>
2024-05-20 15:56:29 -0400

    Fixed App.jsx

--- School Dashboard/src/roots/Home.jsx
+++ School Dashboard/src/roots/Home.jsx
@@ -1,8 +1,7 @@
 import { React, useState } from 'react'
-import './App.css'
 import { Link } from 'react-router-dom';
 
-export default function Home() {
+export const Home = () => {
     
     return (
         <> 

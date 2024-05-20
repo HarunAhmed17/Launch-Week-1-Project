278eaab2085bbb0bd15f5119e6283464d1cc127d
Vento24 <mvento@kountable.com>
2024-05-20 16:50:53 -0400

    add styles folder

--- School Dashboard/src/roots/Home.jsx
+++ School Dashboard/src/roots/Home.jsx
@@ -1,21 +1,17 @@
 import React from 'react';
 import { useState } from 'react';
 import { Link } from 'react-router-dom';
+import Navbar from '../ components/Navbar';
 
 export const Home = () => {
     
     return (
         <> 
-            <h1> Welcome Page </h1>
-            <Link to={"/directory"}> Directory </Link>
-            <br/>
-            <Link to={"/dashboard"}> Dashboard </Link>
-            <br/>
-            <Link to={"/class"}> Class Page </Link>
-            <br/>
-            <Link to={"/calendar"}> Calendar </Link>
-            <br/>
-            
+        <div className='home'>
+            <Navbar/>
+        
+
+        </div>
         </>
     )
 }

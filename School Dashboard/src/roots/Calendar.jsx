b7439587a5cf97b03013270123564382101fcc6d
Sara Inoue <sarainoue11@gmail.com>
2024-05-20 12:41:18 -0800

    currently added calendar title and buttons to update and go between months on the calendar

--- School Dashboard/src/roots/Calendar.jsx
+++ School Dashboard/src/roots/Calendar.jsx
@@ -1,5 +1,41 @@
 import { React } from 'react';
 
+import Navbar from '../ components/Navbar';
+
 export const Calendar = () => {
 
-}
+    const goLeft = () => {
+        // Logic to go to previous month
+    };
+    
+    const goRight = () => {
+        // Logic to go to next month
+    };
+    const getCurrentDate = () => {
+        const currentDate = new Date();
+        const month = currentDate.toLocaleString('default', { month: 'long' });
+        const year = currentDate.getFullYear();
+        return `${month} ${year}`;
+    };
+
+    return (
+    <div>
+
+        <Navbar/>
+   
+        <h1 className='calendartitle'>
+            Calendar
+        </h1>
+              <div className="calendar-navigation">
+                <div className="navigation-item" onClick={goLeft}>
+                    &lt;
+                </div>
+                <div className="navigation-item">{getCurrentDate()}</div>
+                <div className="navigation-item" onClick={goRight}>
+                    &gt;
+                </div>
+
+            </div>
+        </div>
+    );
+};

b5e9a9625b057a720910bc389c64df7cd2238176
mlaflam <mlaflam@users.noreply.github.com>
2024-05-21 15:22:57 -0400

    Editing Basic Table Visuals

--- School Dashboard/src/components/DataTable.jsx
+++ School Dashboard/src/components/DataTable.jsx
@@ -2,14 +2,8 @@ import * as React from 'react';
 import { DataGrid } from '@mui/x-data-grid';
 
 const columns = [
-  { field: 'studentName', headerName: 'First name', width: 130 },
+  { field: 'firstName', headerName: 'First name', width: 130 },
   { field: 'lastName', headerName: 'Last name', width: 130 },
-  {
-    field: 'age',
-    headerName: 'Age',
-    type: 'number',
-    width: 90,
-  },
   {
     field: 'fullName',
     headerName: 'Full name',
@@ -18,32 +12,53 @@ const columns = [
     width: 160,
     valueGetter: (value, row) => `${row.firstName || ''} ${row.lastName || ''}`,
   },
+  {
+    field: 'dob',
+    headerName: 'Date of Birth',
+    sortable: false,
+    width: 190,
+  },
+    {
+    field: 'classes',
+    headerName: 'Enrolled Classes',
+    sortable: false,
+    width: 550,
+  },
 ];
 
 const rows = [
-  // { id: 1, lastName: 'Snow', firstName: 'Jon', age: 35 },
-  // { id: 2, lastName: 'Lannister', firstName: 'Cersei', age: 42 },
-  // { id: 3, lastName: 'Lannister', firstName: 'Jaime', age: 45 },
-  // { id: 4, lastName: 'Stark', firstName: 'Arya', age: 16 },
-  // { id: 5, lastName: 'Targaryen', firstName: 'Daenerys', age: null },
-  // { id: 6, lastName: 'Melisandre', firstName: null, age: 150 },
-  // { id: 7, lastName: 'Clifford', firstName: 'Ferrara', age: 44 },
-  // { id: 8, lastName: 'Frances', firstName: 'Rossini', age: 36 },
-  // { id: 9, lastName: 'Roxie', firstName: 'Harvey', age: 65 },
+  { id: 1, lastName: 'Snow', firstName: 'Jon', dob: '10/9/2000'},
+  { id: 2, lastName: 'Lannister', firstName: 'Cersei', dob: '10/9/2000' },
+  { id: 3, lastName: 'Lannister', firstName: 'Jaime', dob: '10/9/2000' },
+  { id: 4, lastName: 'Stark', firstName: 'Arya', dob: '10/9/2000' },
+  { id: 5, lastName: 'Targaryen', firstName: 'Daenerys', dob: null },
+  { id: 6, lastName: 'Melisandre', firstName: null, dob: '10/9/2000' },
+  { id: 7, lastName: 'Clifford', firstName: 'Ferrara', dob: '10/9/2000' },
+  { id: 8, lastName: 'Frances', firstName: 'Rossini', dob: '10/9/2000' },
+  { id: 9, lastName: 'Roxie', firstName: 'Harvey', dob: '10/9/2000' },
+  { id: 10, lastName: 'Snow', firstName: 'Jon', dob: '10/9/2000'},
+  { id: 11, lastName: 'Lannister', firstName: 'Cersei', dob: '10/9/2000' },
+  { id: 12, lastName: 'Lannister', firstName: 'Jaime', dob: '10/9/2000' },
+  { id: 13, lastName: 'Stark', firstName: 'Arya', dob: '10/9/2000' },
+  { id: 14, lastName: 'Targaryen', firstName: 'Daenerys', dob: null },
+  { id: 15, lastName: 'Melisandre', firstName: null, dob: '10/9/2000' },
+  { id: 16, lastName: 'Clifford', firstName: 'Ferrara', dob: '10/9/2000' },
+  { id: 17, lastName: 'Frances', firstName: 'Rossini', dob: '10/9/2000' },
+  { id: 18, lastName: 'Roxie', firstName: 'Harvey', dob: '10/9/2000' },
 ];
 
 export const DataTable = () => {
   return (
-    <div style={{ height: 550, width: '80%' }} className='datatable'>
+    <div style={{ height: 575, width: '90%' }} className='datatable'>
       <DataGrid
         rows={rows}
         columns={columns}
         initialState={{
           pagination: {
-            paginationModel: { page: 0, pageSize: 5 },
+            paginationModel: { page: 0, pageSize: 10 },
           },
         }}
-        pageSizeOptions={[5, 10]}
+        pageSizeOptions={[10, 20]}
         checkboxSelection
       />
     </div>

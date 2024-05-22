e4a32150bf1b5cb8c2cd5073dbbabe1ce20297f3
Sara Inoue <sarainoue11@gmail.com>
2024-05-22 12:34:01 -0800

    Fully able to take any updates from firebase and update the calendar accordingly.

--- School Dashboard/src/roots/Calendar.jsx
+++ School Dashboard/src/roots/Calendar.jsx
@@ -6,46 +6,84 @@ import timeGridPlugin from '@fullcalendar/timegrid';
 import FullCalendarComponent from '@fullcalendar/react';
 import dayGridPlugin from '@fullcalendar/daygrid';
 
+import { db } from '../firebase.js';
+import { getDocs, collection, query } from "firebase/firestore";
+
+
+
+
 export const Calendar = () => {
+
     const calendarRef = useRef(null);
     const [date, setDate] = useState(new Date());
+    const [startDate, setStartDate] = useState('');
+    const [endDate, setEndDate] = useState('');
+    const [event, setEvent] = useState('');
+    const [isAllDay, setIsAllDay] = useState('');
+    const [allData, setAllData] = useState([]);
 
+    // const handleDateClick = (arg) => {
+    //     alert(arg.dateStr)
+    //   }
 
-    const getCurrentDate = () => {
-        if (calendarRef.current) {
-            const calendarApi = calendarRef.current.getApi();
-            const currentDate = calendarApi.getDate();
-            const month = currentDate.toLocaleString('default', { month: 'long' });
-            const year = currentDate.getFullYear();
-            return `${month} ${year}`;
-        }
-        return '';
+    const fetchData = async () => {
+        let temp = [];
+        const querySnapshot = await getDocs(collection(db, "events"));
+        querySnapshot.forEach((doc) => {
+            temp.push({
+                id: doc.id,
+                ...doc.data(),
+            });
+        });
+
+        const events = temp.map(event => {
+            const { BeginningDate, EndDate, Event, startTime, endTime, AllDay } = event;
+            const start = `${BeginningDate}T${startTime || '00:00:00'}`;
+            const end = `${EndDate ? `${EndDate}T${endTime || '23:23:59'}` : start}`;
+            return {
+                title: Event,
+                start: start,
+                end: end,
+                allDay: AllDay === 'true'
+            };
+        });
+
+        setAllData(events);
+        console.log(allData);
     };
 
     useEffect(() => {
+
         if (calendarRef.current) {
             const calendarApi = calendarRef.current.getApi();
             calendarApi.gotoDate(date);
         }
-    }, [date]);
-
+        fetchData();
+    } ,[]);
     return (
         <div>
             <Navbar />
             <h1>Calendar</h1>
+            <div className='calendarcomponent'>
             <FullCalendarComponent
                 ref={calendarRef}
                 plugins={[dayGridPlugin]}
                 initialView="dayGridMonth"
+                displayEventTime = {true}
+                displayEventEnd = {true}
+                themeSystem='standard'
+                eventMouseEnter={true}
                 headerToolbar={{
                     center: 'title',
                 }}
-                events={[
-                    { title: 'event 1', date: '2024-05-13' , slotDuration:'02:00'},
-                    { title: 'event 2', date: '2024-05-15' }
-                  ]}
+
+         //       dateClick={handleDateClick}
+                events={allData}
+                  //eventDidMount={events}
 
             />
+            </div>
+     
         </div>
     );
 };

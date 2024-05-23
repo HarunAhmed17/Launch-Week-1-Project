d40a086d1d2250a7913dc76b17dfa3fa6f4e6583
Vento24 <mvento@kountable.com>
2024-05-23 14:25:06 -0400

    responsive nav ba r

--- School Dashboard/src/components/Navbar.jsx
+++ School Dashboard/src/components/Navbar.jsx
@@ -3,44 +3,48 @@ import '../index.css';
 import '../styles/directory.css';
 import { Link } from "react-router-dom";
 import logo from '../images/logo.png'
+import { useState } from 'react';
 
 export const Navbar = () => {
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen);
+  };
 
   return (
     <header className="header">
-
-      <div className='left-section'>
-        <div className='title-section'>
-          <img className="header-logo" src={logo} alt="Logo" />
-          <div className='navbar-title'> Thomas Jefferson Elementary School </div>
-        </div>
+      <div className="title-section">
+        <img className="header-logo" src={logo} alt="Logo" />
+        <div className="navbar-title">Thomas Jefferson Elementary School</div>
       </div>
 
-      <div className='right-section'>
-        <Link className='header-link' to={'/'}>Home</Link>
-        <Link className='header-link' to={'/dashboard'}>Dashboard</Link>
-        
-        <div className='directory-link'>
-          <div className='header-link'>Directory</div>
-          
-          <div className='directory-options'>
-            <div className='options-container'>
-              <div className='student-option'>
-                <Link className='student-link' to={'/student-directory'} >Student</Link>
-              </div>
-              <div className='teacher-option'>
-                <Link className='teacher-link' to={'/teacher-directory'}>Teacher</Link>
+      <div className="right-section">
+        <div className={`links-container ${menuOpen ? 'show' : ''}`}>
+          <Link className="header-link" to="/">Home</Link>
+          <Link className="header-link" to="/dashboard">Dashboard</Link>
+          <div className="directory-link">
+            <div className="header-link">Directory</div>
+            <div className="directory-options">
+              <div className="options-container">
+                <div className="student-option">
+                  <Link className="student-link" to="/student-directory">Student</Link>
+                </div>
+                <div className="teacher-option">
+                  <Link className="teacher-link" to="/teacher-directory">Teacher</Link>
+                </div>
               </div>
             </div>
           </div>
-
+          <Link className="header-link" to="/calendar">Calendar</Link>
+          <Link className="header-link-button" to="/login">Log-in</Link>
         </div>
-
-        <Link className='header-link' to={'/calendar'}>Calendar</Link>
-        <Link className='header-link-button' to={'/login'}>Log-in</Link>
+        <div className="hamburger" onClick={toggleMenu}>
+          <div></div>
+          <div></div>
+          <div></div>
+        </div>
       </div>
-      
-    
     </header>
-  )
-}
+  );
+};

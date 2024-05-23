bc9a8fcfb998ce4dca2f581544444106ac2ee900
Abhinav Attaluri <attaluriabhi@gmail.com>
2024-05-23 14:08:56 -0400

    Added functionality to remove classes

--- School Dashboard/src/roots/Dashboard.jsx
+++ School Dashboard/src/roots/Dashboard.jsx
@@ -2,7 +2,7 @@ import { React, useState, useEffect } from 'react';
 import {Navbar} from "../components/Navbar";
 import { db } from "../firebase";
 import "../styles/Dashboard.css";
-import { addDoc, collection, getDocs, query } from "firebase/firestore";
+import { doc, deleteDoc, addDoc, collection, getDocs, query } from "firebase/firestore";
 import { Link } from "react-router-dom";
 import Button from '@mui/material/Button';
 import TextField from '@mui/material/TextField';
@@ -16,15 +16,18 @@ export const Dashboard = () => {
     const [semester, setSemester] = useState("");
     const [color, setColor] = useState("");
 
+    // variable representing id to delete for removeClass
+    const [idToRemove, setIdToRemove] = useState("");
+
     const fetchClasses = async () => {
         try {
             const querySnapshot = await getDocs(query(collection(db, "dashboard")));
-            console.log("Query Snapshot: ", querySnapshot);
+            // console.log("Query Snapshot: ", querySnapshot);
             // const fetchedClasses = [];
             const fetchedClasses = querySnapshot.docs.map(doc => ({id: doc.id, ...doc.data()}));
             setClasses(fetchedClasses);
             console.log("Fetched Classes: ", fetchedClasses);
-            console.log("Size (expected 7): ", querySnapshot.size);
+            console.log("Size: ", querySnapshot.size);
         } catch (error) {
             console.error("Error fetching documents: ", error);
         }
@@ -33,9 +36,6 @@ export const Dashboard = () => {
     useEffect(() => {
         fetchClasses();
     }, [])
-
-
-    // functionality to delete Class
 
     // functionality that adds class
     const handleSubmit = async (e) => {
@@ -53,6 +53,16 @@ export const Dashboard = () => {
         fetchClasses();
     }
 
+
+    // functionality to delete Class
+    const removeClass = async (docId) => {
+        console.log("deleting doc with id: ", docId);
+        await deleteDoc(doc(db, "dashboard", docId));
+        // add fetchClasses(); --> so that it deletes immediately after button is clicked
+        fetchClasses();
+    }
+
+
     return (
         <>
             <Navbar />
@@ -60,38 +70,44 @@ export const Dashboard = () => {
                 <h1> Dashboard </h1>
                 <hr className="line" />
             </div>
-            <div className="classOutline">
-                {classes.map((dashboardClass) => (
-                    // <Link key={dashboardClass.subject} to={`/class/${dashboardClass.subject}`}>
-                        // {console.log(typeof dashboardClass.subject)}
-                        <div key={dashboardClass.id} className="dashboardClass" style={{'--box-color': dashboardClass.color}}> 
-                            <hr className="line" />
-                            <h4> {dashboardClass.subject} </h4>
-                            <h6> {dashboardClass.semester} </h6>
-                        </div>
-                    // </Link>
-                ))}
-            </div>
             {/* form should be outside the main classOutline div */}
             <div class="form"> 
                     <form className="addClassForm" onSubmit={handleSubmit}> 
+                        <Button color="success" variant="contained" type="submit"> Add Class </Button>
+                        <br/>
                         <label> Subject: </label>
                         <TextField id="outlined-basic" lable="Outlined" variant="outlined" 
                         onChange={(e) => setSubject(e.target.value)} />
                         <br/>
-
                         <label> Semester: </label>
                         <TextField id="outlined-basic" lable="Outlined" variant="outlined" 
                         onChange={(e) => setSemester(e.target.value)} />
                         <br/>
-
                         <label> Color: </label>
                         <TextField id="outlined-basic" lable="Outlined" variant="outlined"
                         onChange={(e) => setColor(e.target.value)} />
-
-                        <Button variant="Contained" type="submit"> Add Class </Button>
+                        <br/> 
                     </form>
             </div>
+            <div className="classOutline">
+                {classes.map((dashboardClass) => (
+                    <div key={dashboardClass.id} className="dashboardClass" style={{'--box-color': dashboardClass.color}}> 
+                        <Link key={dashboardClass.subject} to={`/class/${dashboardClass.subject}`}>
+                            {console.log(typeof dashboardClass.subject)}
+                            <hr className="line" />
+                            <h4> {dashboardClass.subject} </h4>
+                            <h6> {dashboardClass.semester} </h6>
+                        </Link>
+                        <Button size="small" color="success" variant="outlined" type="submit"
+                        onClick={(e) => {
+                            e.preventDefault();
+                            removeClass(dashboardClass.id)
+                        }}> Remove Class </Button>
+                    </div>
+                    
+                ))}
+            </div>
+            
         </>
     );
 };

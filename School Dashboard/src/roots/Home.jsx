cdc1e6ba902ecc5c64356dc41c1cb4a064fa6f55
Vento24 <mvento@kountable.com>
2024-05-24 09:45:30 -0400

    home screen mobile responsiveness

--- School Dashboard/src/roots/Home.jsx
+++ School Dashboard/src/roots/Home.jsx
@@ -8,39 +8,47 @@ import curiosity from '../images/curiosity.jpg'
 import excellence from '../images/excellence.jpg'
 
 export const Home = () => {
-    
     return (
         <>
-        <Navbar/>
-        <div className='home'>
-           
-           <div className='title-logo'>
-                
-                <div className='main-title'> <h1> &nbsp;Thomas Jefferson <br/> Elementary School</h1> <h2> Inspiring Young Minds Every Day</h2></div>
-                <img className='large-logo' src={logo} alt='logo'/>
-                
-           </div>
-
-           {/* <div className='images'>
-                <div > 
-                    <div className='curiosity-container'>
-                        <h2> Curiosity </h2>
-                        <img src={curiosity} alt='curiosity'/>
+            <Navbar />
+            <div className='home'>
+                <div className='title-logo'>
+                    <div className='main-title'>
+                        <h1> &nbsp;Thomas Jefferson <br /> Elementary School</h1>
+                        <h2> Inspiring Young Minds Every Day</h2>
                     </div>
+                    <img className='large-logo' src={logo} alt='logo' />
                 </div>
-                <div className='col-imgs'> 
-                    <div className='excellence-container'>
+                <div className='images'>
+                    <div className='curiosity-container'>
+                        <h2> Curiosity </h2> 
+                        <img className='curiosity' src={curiosity} alt='curiosity' />
+                    </div>
+                    <div className='col-imgs curiosity-container'>
+                             <div className='excellence-container'>
                         <h2> Excellence </h2>
-                        <img className='excellence' src={excellence} alt='excellence'/>
+                        <img className='excellence' src={excellence} alt='excellence' />
                     </div>
                     <div className='collaboration-container'>
-                    <h2> Collaboration </h2>
-                        <img className='collaboration' src={collaboration} alt='collaboration'/>
+                        <h2> Collaboration </h2>
+                        <img className='collaboration' src={collaboration} alt='collaboration' />
+                    </div> 
                     </div>
                 </div>
-               
-           </div> */}
-        </div>
+                <div className='imgs-contain'>
+                    <div className='carousel-slide'>
+                        <div className='curiosity-contain'>
+                            <div className='carousel-text'>Curiosity</div>
+                        </div>
+                        <div className='excellence-contain'>
+                            <div className='carousel-text'>Excellence</div>
+                        </div>
+                        <div className='collaboration-contain'>
+                            <div className='carousel-text'>Collaboration</div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </>
-    )
-}
+    );
+};
